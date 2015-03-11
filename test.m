758a14aeab15d6ec5e20daec9e71ad05e1c5fc4b
lizzyburl <lizzyburl@gmail.com>
2015-03-11 00:33:50 -0400

    First Commit

---
+++ test.m
@@ -0,0 +1,11 @@
+clear;
+CreateWinterGif('testPic.jpg');
+% [rows, cols, three] = size(Im);
+% filter = zeros(rows, cols, three);
+% G = fspecial('gaussian', [5 5],1)*;
+% a =  filter(71:75, 211:215, :);
+% b = repmat(G,1,1,three);
+% filter(71:75, 211:215, :) = a + b;
+% ImToDisplay = Im/255.+ filter;
+% imshow(ImToDisplay);
+
